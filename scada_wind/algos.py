db410e6fc95e8102b160416f87a76b0bd0689fba
Nigel Cleland <nigel.cleland@gmail.com>
2013-10-17 09:55:47 +1300

    Initial processing algo done, this looks at changes in five minute wind data
    If the change is larger than forty percent of the total capacity of the wind
    farms output for that period (relative basis, moving target) then it
    assesses that the period may be of interest. It will take a ten minute
    slice either side of this and save the result to a file for later processing

---
+++ scada_wind/algos.py
@@ -0,0 +1,133 @@
+"""
+Processing stages to work with the four second wind data
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import glob
+import datetime
+from pandas.tseries.offsets import Minute
+
+def load_month(filename):
+    """ Load a months worth of data and handle indices """
+
+    df = pd.read_csv(filename, index_col=0)
+    df.index = convert_datetime(df["Timestamp"])
+    return df
+
+def convert_datetime(series):
+    time_format = "%d-%b-%y %H:%M:%S"
+    return pd.to_datetime(series, format=time_format)
+
+def wind_site_selector(columns, site):
+
+    wind_farms = {"West Wind": ["WWD"],
+                 "Tararua": ["TWF"],
+                 "All Tararua": ["TWF", "TAP", "TWC"],
+                 "White Hill": ["WHL"],
+                 "Te Uku": ["TUK"],
+                 "All": ["GENERATION"],
+                 "North Island": ["TWF", "TAP", "TWC", "WWD", "TRH", "TUK"],
+                 "South Island": ["WHL", "MAH"]}
+
+    return [x for x in columns if any(y in x for y in wind_farms[site])]
+
+def aggregate_farms(df, cols):
+    return df[cols].sum(axis=1)
+
+def five_min_resample(series):
+    return series.resample("5min", how="mean")
+
+def five_min_delta(series):
+    cutter = lambda x: x[-1] - x[0]
+    return pd.rolling_apply(series, 2, func=cutter)
+
+def five_min_cutoff(series, rel_cutoff=0.4):
+    cutoff = rel_cutoff * series.max() * -1
+    cutoff_points = series[series <= cutoff
+    cutoff_points.sort()
+    return cutoff_points
+
+def offsetter(x, minute=10):
+    return (x - Minute(minute), x + Minute(minute))
+
+def slicer(series, stamp):
+    begin, end = offsetter(stamp)
+    return series[begin:end]
+
+def all_slicer(series, stamps):
+    return [slicer(series, stamp) for stamp in stamps]
+
+def process_series(series):
+    five_minute = five_min_resample(series)
+    deviation = five_min_delta(series)
+    cutoff = five_min_cutoff(deviation)
+    stamps = cutoff.index
+    return all_slicer(series, stamps)
+
+def create_directory(filename):
+    cwd = os.getcwd()
+    new_name = os.path.join(cwd, os.path.splitext(filename)[0])
+    if not os.path.exists(new_name):
+        os.mkdir(new_name)
+
+    return new_name
+
+def create_farm_directory(dir, farm):
+    new_name = os.path.join(dir, farm.replace(' ', ''))
+    if not os.path.exists(new_name):
+        os.mkdir(new_name)
+    return new_name
+
+def process_save(df, farm, farm_dir):
+
+    try:
+        wind_series = aggregate_farms(df, wind_site_selector(df.columns, farm))
+        sliced_series = process_series(wind_series)
+
+        for i, possible in enumerate(sliced_series):
+
+            opt = "%s_Option_%s.csv" % (farm.replace(' ', '_'), i)
+            savename = os.path.join(farm_dir, opt)
+            possible.name = "Scada Output"
+            possible.index.name = "Timestmap"
+            possible.to_csv(savename, index=True, header=True)
+    except:
+        print "There was an Error processing", farm
+
+
+def process_month(filename):
+
+    df = load_month(filename)
+    directory = create_directory(filename)
+
+    wind_farms = ("West Wind", "Tararua", "Te Apiti", "All Tararua",
+                  "White Hill", "Te Uku", "All", "North Island",
+                  "South Island")
+
+    for farm in wind_farms:
+        farm_dir = create_farm_directory(directory, farm)
+        process_save(df, farm_dir, farm)
+
+def run_directory(directory):
+
+    files = glob.glob(os.path.join(directory, "*.csv"))
+    for f in files:
+        print f, "Begun"
+        try:
+            process_month(f)
+            print f, "Complete"
+        except:
+            print f, "Unable to complete, investiate further"
+
+
+
+
+
+
+
+
+
+
+
